fe9f257e655341a2ef3625cf0a97000b83de8567
limseonghyun <sksrpf1126@naver.com>
2023-02-27 22:37:12 +0900

    Feat : 프로젝트 기본 세팅 적용

---
+++ vroomvroom/src/main/java/com/backend/vroomvroom/VroomvroomApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vroomvroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomvroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomvroomApplication.class, args);
+	}
+
+}
