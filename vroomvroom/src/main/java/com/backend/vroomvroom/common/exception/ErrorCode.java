d32616aa3124b00ec5436150f9a368d051779f11
limseonghyun <sksrpf1126@naver.com>
2023-04-03 23:05:26 +0900

    Feat : 메서드 방식의 인증 - 인가에 대한 예외처리 구현

--- vroomvroom/src/main/java/com/backend/vroomvroom/common/exception/ErrorCode.java
+++ vroomvroom/src/main/java/com/backend/vroomvroom/common/exception/ErrorCode.java
@@ -12,6 +12,12 @@ public enum ErrorCode {
     DUPLICATED_ENTITY(HttpStatus.BAD_REQUEST, "이미 존재하는 엔티티인 경우"),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청한 값이 올바르지 않은 경우"),
 
+    //401번 에러
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증에 실패한 경우"),
+
+    //403번 에러
+    ROLE_NOT_EXISTS(HttpStatus.FORBIDDEN, "권한이 없는 경우"),
+
     //404번 에러
     NOT_FOUND_ENTITY(HttpStatus.NOT_FOUND, "존재하지 않는 엔티티인 경우"),
 
--- vroomvroom/src/main/java/com/backend/vroomvroom/common/exception/GlobalExceptionHandler.java
+++ vroomvroom/src/main/java/com/backend/vroomvroom/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.backend.vroomvroom.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -39,6 +42,22 @@ public class GlobalExceptionHandler {
         return ErrorResponse.toResponseEntity(INVALID_REQUEST, builder.toString());
     }
 
+    /**
+     * 인증, 인가에 대한 예외 처리
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+        log.error("handleAccessDeniedException throw Exception : {}", e);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if(auth == null || auth.getPrincipal().equals("anonymousUser")) {
+            return ErrorResponse.toResponseEntity(UNAUTHORIZED, "로그인이 필요한 서비스입니다.");
+        } else {
+            return ErrorResponse.toResponseEntity(ROLE_NOT_EXISTS, "해당 계정은 권한이 없습니다.");
+        }
+
+    }
 
     /**
      * 예상하지 못한 예외가 발생할 경우
@@ -48,4 +67,5 @@ public class GlobalExceptionHandler {
         log.error("handleException throw Exception : {}", e);
         return ErrorResponse.toResponseEntity(INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다.");
     }
+
 }
